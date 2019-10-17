b325c32c497de61c30e2f221aead2a818b5a89a7
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2019-10-17 19:17:24 +0530

    Issue#9 - Added validation to missing places - Checkbox

--- library/checkbox/checkbox.js
+++ library/checkbox/checkbox.js
@@ -1,27 +1,42 @@
+import { isNullOrUndefined } from "util";
+
 export class Checkbox {
     constructor() { }
     // Add event listeners to all checkboxes on the page
     static initialize() {
       console.log("Initializing checkbox UI  (convert all '.checkbox' to a slider UI control)");
       // Attach event listener to the checkbox
-      document.querySelectorAll(".checkbox").forEach(function(obj) {
+      let matchingCheckbox = document.querySelectorAll('checkbox');
+      matchingCheckbox.forEach(function(obj) {
         obj.addEventListener("click", function(event) {
           let state = this.getAttribute("data-value");
-          state == "off" ? this.setAttribute("data-value", "on") : this.setAttribute("data-value", "off");
+          if (isNullOrUndefined(state)){
+            throw Error("Checkbox value seems to be nil or empty");
+          }
+          state === "off" ? this.setAttribute("data-value", "on") : this.setAttribute("data-value", "off");
         });
       });
       // Attach event listener to the checkbox label
-      document.querySelectorAll(".checkbox-label").forEach(function(obj) {
-        let f = obj.getAttribute("for");
-        if (f) {
-          obj.addEventListener("click", function(event) {
-            let checkbox = document.getElementById(f);
-            if (checkbox) {
-                let state = checkbox.getAttribute("data-value");
-                state == "off" ? checkbox.setAttribute("data-value", "on") : checkbox.setAttribute("data-value", "off");
-            }
-          });
+      let matchingCheckboxLabel = document.querySelectorAll('checkbox-label');
+      matchingCheckboxLabel.forEach(function(obj) {
+        let labelAttribute = obj.getAttribute("for");
+        if(isNullOrUndefined(labelAttribute)){
+          throw Error("label value seems to be emtpy or nil")
         }
+
+        obj.addEventListener("click", function(event) {
+          let checkbox = document.getElementById(labelAttribute);
+          // What if there's no such checkbox.
+          if(isNullOrUndefined(checkbox)){
+            throw Error("Cannot able to find checkbox.");
+          }
+          let state = checkbox.getAttribute("data-value")
+          if (isNullOrUndefined(state)){
+            throw Error("Checkbox value seems to be undefined or nil");
+          }
+          state === "off" ? checkbox.setAttribute("data-value", "on") :
+              checkbox.setAttribute("data-value", "off");
+        });
       });
     }
 };
